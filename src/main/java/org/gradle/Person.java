f080a4b4b61b8a094aeddc3074ffc389a17b1364
Pavel Kalashnikov <p.kalashnikov@epages.com>
2016-04-04 14:57:15 +0200

    CST-2032: dependency added

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -1,16 +1,8 @@
 package org.gradle;
 
-import org.apache.commons.collections.list.GrowthList;
-
-public class Person {
-    private final String name;
+public class Person extends MainPerson {
 
     public Person(String name) {
-        this.name = name;
-        new GrowthList();
-    }
-
-    public String getName() {
-        return name;
+        super(name);
     }
 }
